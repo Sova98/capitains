80984ee57c3b66a59b7e40a5d0c7301abf5d75ab
Sova98 <marina.95.m@mail.ru>
2018-06-12 13:01:45 +0300

    added detailed div for awards

--- app/static/js/new_home.js
+++ app/static/js/new_home.js
@@ -1,4 +1,6 @@
 
+var all_data;
+
 getTasks('karate_type')
 $('#karate_type').addClass("active");
 
@@ -6,7 +8,65 @@ $(".btn").click(function(){
   $(".btn").removeClass("active");
   $(this).addClass("active");
   getTasks($(this).attr("id"))
-})
+});
+
+$(".content").on("click", 'div.new_award', function(){
+
+	var title_text = $(this).children().first().text();
+	var title = document.createTextNode(title_text);
+	var container;
+	for(var i = 1; i < parseInt(all_data[0]); i++){
+		if(title_text == all_data[i]['title']){
+			container = all_data[i]['container'].split('/');
+		}
+	}
+	var award_detail = document.createElement('div');
+	award_detail.className = 'award_detail_container';
+	var close = document.createElement('div');
+	var close_sign = document.createTextNode('×');
+	close.className = 'closeDiv';
+	close.appendChild(close_sign);
+
+	var h4 = document.createElement('H4');
+	var btn = document.createElement('button');
+	var div_btn = document.createElement('div');
+	var ul = document.createElement('ul')
+	ul.className = 'list-group'
+	div_btn.className = 'div_btn';
+	btn.innerHTML = 'получить';
+	btn.className = 'btn_getAward';
+	div_btn.appendChild(btn);
+
+	h4.appendChild(title);
+	for(var q = 0; q < container.length; q++){
+		if(container[q] != ''){
+			var p = document.createElement("p");
+			p.appendChild(document.createTextNode(container[q]));
+			var li = document.createElement('li');
+			li.className = 'list-group-item font'
+			li.appendChild(p);
+			ul.appendChild(li);
+		}
+	}
+
+	var div = document.createElement('DIV');
+	div.className = 'award_detail';  
+	div.appendChild(h4);
+	div.appendChild(ul);
+	div.appendChild(div_btn);
+
+	award_detail.append(div); 
+	award_detail.appendChild(close);
+
+	$('.outPopUp').append(award_detail);
+	$('body').append('<span class="mask"></span>');
+});
+
+
+$(".outPopUp").on("click", "div.closeDiv", function(){
+    $('.award_detail_container').remove();
+    $('.mask').remove();
+});
 
 function getTasks(type){
 	$.post('/tasksSpecified', {
@@ -15,18 +75,24 @@ function getTasks(type){
 		$('.awards_block').remove();
 		var awards_block = document.createElement('div');
 		awards_block.className = 'awards_block';
+		all_data = resp;
 		for(var i = 1; i < parseInt(resp[0]); i++){
 			var title = document.createTextNode(resp[i]['title']);
-			var container = document.createTextNode(resp[i]['container']);
-			var p = document.createElement("p");  
 			var h4 = document.createElement('H4');
+			var btn = document.createElement('button');
+			var div_btn = document.createElement('div');
+			var ul = document.createElement('ul')
+			ul.className = 'list-group'
+			div_btn.className = 'div_btn';
+			btn.innerHTML = 'получить';
+			btn.className = 'btn_getAward';
+			div_btn.appendChild(btn);			
 			h4.appendChild(title);
-			p.appendChild(container);
-
 			var div = document.createElement('DIV');
 			div.className = 'new_award';  
 			div.appendChild(h4);
-			div.appendChild(p);
+			div.appendChild(ul);
+			div.appendChild(div_btn);
 			awards_block.appendChild(div); 
 		}
 		$('.content').prepend(awards_block); 
